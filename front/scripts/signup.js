80a2a140f5eea7f4025d0a90ef332c759a47e660
h0pped <notawril@gmail.com>
2021-12-26 21:23:27 +0200

    feat: add image preview & form user data that are ready to send

--- front/scripts/signup.js
+++ front/scripts/signup.js
@@ -3,7 +3,11 @@ const genderSelect = document.querySelector("#gender-input");
 const cityList = document.querySelector("#cityname");
 const cityInput = document.querySelector("#city-input");
 const SERVER_URL = "http://127.0.0.1:3000";
+const signUpForm = document.querySelector("#signup-form");
+const inputs = signUpForm.elements;
+const imageInput = document.querySelector("#image-input");
 
+const imagePreview = document.querySelector(".image-preview");
 const loadCountries = () => {
   fetch(`${SERVER_URL}/countries/all`)
     .then((data) => data.json())
@@ -34,10 +38,10 @@ const loadGenders = () => {
   fetch(`${SERVER_URL}/genders/all`)
     .then((data) => data.json())
     .then((data) => {
-      data.forEach((city) => {
+      data.forEach((gender) => {
         let genderOption = document.createElement("option");
         genderOption.setAttribute("value", gender.ID);
-        genderOption.innerHTML = city.title;
+        genderOption.innerHTML = gender.title;
         genderSelect.appendChild(genderOption);
       });
     });
@@ -48,6 +52,44 @@ countrySelect.addEventListener("change", (e) => {
   cityInput.value = "";
   loadCities(e.target.value);
 });
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.onerror = (err) => {
+      reject("Error: ", error);
+    };
+  });
+}
 
+signUpForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  console.log("processing...");
+  const img = await getBase64(inputs["image-input"].files[0]);
+  const user = {
+    avatar: img,
+    name: inputs["name-input"].value,
+    surname: inputs["surname-input"].value,
+    email: inputs["email-input"].value,
+    password: inputs["password-input"].value,
+    countryID: inputs["country-input"].value,
+    city: inputs["city-input"].value,
+    genderID: inputs["gender-input"].value,
+    street: inputs["street-input"].value,
+    postalCode: inputs["postal-input"].value,
+    description: inputs["description-input"].value,
+  };
+  console.log(user);
+});
+imageInput.addEventListener("change", (e) => {
+  imagePreview.src = URL.createObjectURL(e.target.files[0]);
+  imagePreview.onload = function () {
+    URL.revokeObjectURL(imagePreview.src);
+    document.querySelector("#image-preview-text").style.display = "none";
+  };
+});
 loadCountries();
 loadGenders();
