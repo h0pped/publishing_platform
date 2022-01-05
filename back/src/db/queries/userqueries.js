3ebcf8a460f38fa6dd57c05a8098a4dd8cf481c4
h0pped <notawril@gmail.com>
2022-01-05 16:18:15 +0200

    feat: add route to get profile info by id

--- back/src/db/queries/userqueries.js
+++ back/src/db/queries/userqueries.js
@@ -3,6 +3,8 @@ export const getAllUsers = () =>
 
 export const findByEmail = (email) =>
   `Select User.id, User.name,User.surname,User.email, User.profile_description,User.password,User.avatar_path,Gender.title as "Gender", userStatus.title as "Status", userRole.title as "Role" from User inner join userStatus on userStatus.id=User.status_id inner join userRole on userRole.id=User.role_id inner join Gender on Gender.id=User.gender_id where User.email='${email}'; `;
+export const findByID = (id) =>
+  `Select User.id, User.name,User.surname,User.email, User.profile_description,User.password,User.avatar_path,Gender.title as "Gender", userStatus.title as "Status", userRole.title as "Role" from User inner join userStatus on userStatus.id=User.status_id inner join userRole on userRole.id=User.role_id inner join Gender on Gender.id=User.gender_id where User.ID='${id}'; `;
 
 export const insertNewUser = (
   user
