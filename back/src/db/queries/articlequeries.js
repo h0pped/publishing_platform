b52e6763dd10c7f3adf9765a37a7759ef93e1be8
h0pped <notawril@gmail.com>
2022-01-06 14:24:52 +0200

    get articles info

--- back/src/db/queries/articlequeries.js
+++ back/src/db/queries/articlequeries.js
@@ -1,2 +1,15 @@
 export const getRecentArticles = () =>
   'Select * from Article where status_id=(select id from ArticleStatus where title="Active") order by postDate DESC LIMIT 9;';
+
+export const getUserArticles = (email) =>
+  `Select * from Article where user_id=(select id from user where email="${email}") and status_id=(select id from ArticleStatus where title="Active") order by postDate DESC;`;
+
+export const getArticleCategory = (
+  articleId
+) => `select Article.ID as "article_id", Category.ID as "category_id", Category.Title, Category.thumbnail_path from Article
+inner join Category on Article.category_id = Category.ID where Article.ID = ${articleId}`;
+
+export const getArticleTags = (articleId) => `
+Select tag.id as "tag_id",tag.title as "Tag" from Article_Tag a_t
+inner join Tag as tag on a_t.tag_id=tag.id
+where article_id=${articleId};`;
--- back/src/routes/articleRoutes.js
+++ back/src/routes/articleRoutes.js
@@ -10,4 +10,75 @@ router.get("/articles/recent", (req, res) => {
   });
 });
 
+const getArticleCategory = (articleID) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      articleQueries.getArticleCategory(articleID),
+      (err, rows, fields) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows[0]);
+      }
+    );
+  });
+};
+const getArticleTags = (articleId) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      articleQueries.getArticleTags(articleId),
+      (err, rows, fields) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows);
+      }
+    );
+  });
+};
+const getUserArticles = (email) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      articleQueries.getUserArticles(email),
+      (err, rows, fields) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows);
+      }
+    );
+  });
+};
+router.get("/articles/byUser/:email", (req, res) => {
+  let { email } = req.params;
+  getUserArticles(email)
+    .then((rows) => {
+      let articles = rows;
+      Promise.all(
+        articles.map((item) => {
+          return getArticleCategory(item.ID);
+        })
+      ).then((data) => {
+        console.log(data);
+        articles = articles.map((el, index) => {
+          el.category = data[index];
+          return el;
+        });
+        Promise.all(
+          articles.map((item) => {
+            return getArticleTags(item.ID);
+          })
+        )
+          .then((tagsr) => {
+            articles = articles.map((item, index) => {
+              item.tags = tagsr[index];
+              return item;
+            });
+            return res.send({ articles });
+          })
+          .catch((err) => res.status(500).send({ err }));
+      });
+    })
+    .catch((err) => res.status(500).send({ err }));
+});
 export default router;
