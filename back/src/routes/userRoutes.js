6d94b89a3f7189f97f70d9096c87fbadfc7712a4
h0pped <notawril@gmail.com>
2022-01-09 13:56:44 +0200

    fix: registration promises

--- back/src/routes/userRoutes.js
+++ back/src/routes/userRoutes.js
@@ -41,7 +41,67 @@ router.post("/users/findByCredentials", (req, res) => {
     }
   );
 });
-
+const checkCity = (countryID, city) => {
+  return new Promise((resolve, reject) => {
+    let connection = connectionRequest.connectionRequest();
+    connection.query(
+      cityQueries.getByCountryAndTitle(countryID, city),
+      (err, res, fields) => {
+        if (err) {
+          reject(err);
+          return connection.destroy();
+        }
+        resolve(res[0]);
+        return connection.destroy();
+      }
+    );
+  });
+};
+const insertCity = (countryID, city) => {
+  return new Promise((resolve, reject) => {
+    let connection = connectionRequest.connectionRequest();
+    connection.query(
+      cityQueries.insertCity(countryID, city),
+      (err, res, fields) => {
+        if (err) {
+          reject(err);
+          return connection.destroy();
+        }
+        resolve(res[0]);
+        return connection.destroy();
+      }
+    );
+  });
+};
+const insertNewUser = (user) => {
+  return new Promise((resolve, reject) => {
+    let connection = connectionRequest.connectionRequest();
+    connection.query(userQueries.insertNewUser(user), (err, res, fields) => {
+      if (err) {
+        reject(err);
+        return connection.destroy();
+      }
+      resolve(res[0]);
+      return connection.destroy();
+    });
+  });
+};
+const insertLocation = (location) => {
+  return new Promise((resolve, reject) => {
+    let connection = connectionRequest.connectionRequest();
+    connection.query(
+      locationQueries.insertNewLocation(location),
+      (err, res, fields) => {
+        if (err) {
+          reject(err);
+          return connection.destroy();
+        }
+        resolve(res[0]);
+        return connection.destroy();
+      }
+    );
+  });
+};
 router.post("/users/signup", (req, res) => {
   const user = req.body;
   let regex;
@@ -51,133 +111,199 @@ router.post("/users/signup", (req, res) => {
   let buffer;
   let imagepath = "";
 
-  if (user.avatar) {
-    try {
-      regex = /^data:.+\/(.+);base64,(.*)$/;
-      matches = user.avatar.match(regex);
-      ext = matches[1];
-      data = matches[2];
-      buffer = Buffer.from(data, "base64");
+  if (user) {
+    if (user.avatar) {
+      try {
+        regex = /^data:.+\/(.+);base64,(.*)$/;
+        matches = user.avatar.match(regex);
+        ext = matches[1];
+        data = matches[2];
+        buffer = Buffer.from(data, "base64");
 
-      imagepath = user.email + "_avatar." + ext;
-      fs.writeFileSync(`./public/profile_pics/${imagepath}`, buffer);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  let connection = connectionRequest.connectionRequest();
-  connection.query(
-    //check if city exists in db
-    cityQueries.getByCountryAndTitle(user.countryID, user.city),
-    (err, dbres, fields) => {
-      console.log(err, dbres);
-      if (err === null) {
-        const city = dbres[0];
-        if (!city) {
-          //insert city
-          connection.query(
-            cityQueries.insertCity(user.countryID, user.city),
-            (cityerr, citydbres, cityfields) => {
-              if (cityerr === null) {
-                let salt = bcrypt.genSaltSync(10);
-                let hash = bcrypt.hashSync(user.password, salt);
-                //add user
-                const userdb = {
-                  name: user.name,
-                  surname: user.surname,
-                  email: user.email,
-                  password: hash,
-                  description: user.description,
-                  genderID: user.genderID,
-                  imagepath,
-                };
-                const userlocation = {
-                  countryID: user.countryID,
-                  city: user.city,
-                  postalCode: user.postalCode,
-                  street: user.street,
-                  userEmail: user.email,
-                };
-                connection.query(
-                  userQueries.insertNewUser(userdb),
-                  (usererr, userdbres, userfields) => {
-                    if (usererr === null) {
-                      connection.query(
-                        locationQueries.insertNewLocation(userlocation),
-                        (locationerr, locationdbres, locationfields) => {
-                          if (locationerr === null) {
-                            res.status(201).send(user);
-                            return connection.destroy();
-                          } else {
-                            console.log(locationerr);
-                            res.send({ err: locationerr });
-                            return connection.destroy();
-                          }
-                        }
-                      );
-                    } else {
-                      res.status(400).send({ err: usererr.sqlMessage });
-                      return connection.destroy();
-                    }
-                  }
-                );
-              }
-            }
-          );
-        } else {
-          let salt = bcrypt.genSaltSync(10);
-          let hash = bcrypt.hashSync(user.password, salt);
-          //add user
-          const userdb = {
-            name: user.name,
-            surname: user.surname,
-            email: user.email,
-            password: hash,
-            description: user.description,
-            genderID: user.genderID,
-            imagepath,
-          };
-          const userlocation = {
-            countryID: user.countryID,
-            city: user.city,
-            postalCode: user.postalCode,
-            street: user.street,
-            userEmail: user.email,
-          };
-          connection.query(
-            userQueries.insertNewUser(userdb),
-            (usererr, userdbres, userfields) => {
-              if (usererr === null) {
-                connection.query(
-                  locationQueries.insertNewLocation(userlocation),
-                  (locationerr, locationdbres, locationfields) => {
-                    if (locationerr === null) {
-                      res.status(201).send(user);
-                      return connection.destroy();
-                    } else {
-                      console.log(locationerr);
-                      res.send({ err: locationerr });
-                      return connection.destroy();
-                    }
-                  }
-                );
-              } else {
-                console.log("err");
-
-                res.status(400).send({ err: usererr.sqlMessage });
-                return connection.destroy();
-              }
-              connection.destroy();
-            }
-          );
-        }
-      } else {
+        imagepath = user.email + "_avatar." + ext;
+        fs.writeFileSync(`./public/profile_pics/${imagepath}`, buffer);
+      } catch (err) {
         console.log(err);
       }
     }
-  );
-  connection.destroy();
+    checkCity(user.countryID, user.city)
+      .then((data) => {
+        let salt = bcrypt.genSaltSync(10);
+        let hash = bcrypt.hashSync(user.password, salt);
+        const userdb = {
+          name: user.name,
+          surname: user.surname,
+          email: user.email,
+          password: hash,
+          description: user.description,
+          genderID: user.genderID,
+          imagepath,
+        };
+        if (!data) {
+          // Insert new City
+          insertCity(user.countryID, user.city).then((d) => {
+            return insertNewUser(userdb);
+          });
+        } else {
+          return insertNewUser(userdb);
+        }
+      })
+      .then((data) => {
+        const userlocation = {
+          countryID: user.countryID,
+          city: user.city,
+          postalCode: user.postalCode,
+          street: user.street,
+          userEmail: user.email,
+        };
+        return insertLocation(userlocation);
+      })
+      .then((data) => {
+        res.status(201).send(user);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send({ err });
+      });
+  } else {
+    res.status(400).send({ err: "User is requires" });
+  }
 });
+// router.post("/users/signup", (req, res) => {
+//   const user = req.body;
+//   let regex;
+//   let matches;
+//   let ext;
+//   let data;
+//   let buffer;
+//   let imagepath = "";
+
+//   if (user.avatar) {
+//     try {
+//       regex = /^data:.+\/(.+);base64,(.*)$/;
+//       matches = user.avatar.match(regex);
+//       ext = matches[1];
+//       data = matches[2];
+//       buffer = Buffer.from(data, "base64");
+
+//       imagepath = user.email + "_avatar." + ext;
+//       fs.writeFileSync(`./public/profile_pics/${imagepath}`, buffer);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+//   let connection = connectionRequest.connectionRequest();
+//   connection.query(
+//     //check if city exists in db
+//     cityQueries.getByCountryAndTitle(user.countryID, user.city),
+//     (err, dbres, fields) => {
+//       console.log(err, dbres);
+//       if (err === null) {
+//         const city = dbres[0];
+//         if (!city) {
+//           //insert city
+//           connection.query(
+//             cityQueries.insertCity(user.countryID, user.city),
+//             (cityerr, citydbres, cityfields) => {
+//               if (cityerr === null) {
+//                 let salt = bcrypt.genSaltSync(10);
+//                 let hash = bcrypt.hashSync(user.password, salt);
+//                 //add user
+//                 const userdb = {
+//                   name: user.name,
+//                   surname: user.surname,
+//                   email: user.email,
+//                   password: hash,
+//                   description: user.description,
+//                   genderID: user.genderID,
+//                   imagepath,
+//                 };
+//                 const userlocation = {
+//                   countryID: user.countryID,
+//                   city: user.city,
+//                   postalCode: user.postalCode,
+//                   street: user.street,
+//                   userEmail: user.email,
+//                 };
+//                 connection.query(
+//                   userQueries.insertNewUser(userdb),
+//                   (usererr, userdbres, userfields) => {
+//                     if (usererr === null) {
+//                       connection.query(
+//                         locationQueries.insertNewLocation(userlocation),
+//                         (locationerr, locationdbres, locationfields) => {
+//                           if (locationerr === null) {
+//                             res.status(201).send(user);
+//                             return connection.destroy();
+//                           } else {
+//                             console.log(locationerr);
+//                             res.send({ err: locationerr });
+//                             return connection.destroy();
+//                           }
+//                         }
+//                       );
+//                     } else {
+//                       res.status(400).send({ err: usererr.sqlMessage });
+//                       return connection.destroy();
+//                     }
+//                   }
+//                 );
+//               }
+//             }
+//           );
+//         } else {
+//           let salt = bcrypt.genSaltSync(10);
+//           let hash = bcrypt.hashSync(user.password, salt);
+//           //add user
+//           const userdb = {
+//             name: user.name,
+//             surname: user.surname,
+//             email: user.email,
+//             password: hash,
+//             description: user.description,
+//             genderID: user.genderID,
+//             imagepath,
+//           };
+//           const userlocation = {
+//             countryID: user.countryID,
+//             city: user.city,
+//             postalCode: user.postalCode,
+//             street: user.street,
+//             userEmail: user.email,
+//           };
+//           connection.query(
+//             userQueries.insertNewUser(userdb),
+//             (usererr, userdbres, userfields) => {
+//               if (usererr === null) {
+//                 connection.query(
+//                   locationQueries.insertNewLocation(userlocation),
+//                   (locationerr, locationdbres, locationfields) => {
+//                     if (locationerr === null) {
+//                       res.status(201).send(user);
+//                       return connection.destroy();
+//                     } else {
+//                       console.log(locationerr);
+//                       res.send({ err: locationerr });
+//                       return connection.destroy();
+//                     }
+//                   }
+//                 );
+//               } else {
+//                 console.log("err");
+
+//                 res.status(400).send({ err: usererr.sqlMessage });
+//                 return connection.destroy();
+//               }
+//               connection.destroy();
+//             }
+//           );
+//         }
+//       } else {
+//         console.log(err);
+//       }
+//     }
+//   );
+// });
 
 const findUserByEmail = (email) => {
   return new Promise((resolve, reject) => {
